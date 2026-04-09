cb267c9b1139b7068f56c134a7e30605630b5089
James Knepper <jwknepper@ucdavis.edu>
2026-04-08 18:22:37 -0700

    Update CRC and pull usart refs into #defs

--- Src/main.c
+++ Src/main.c
@@ -4,7 +4,7 @@
 #include "Timers/timers.h"
 #include "UART/mgt_handler.h"
 
-
+#define PFC_USART USART1
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -13,7 +13,7 @@
 int main(void)
 {
   init_platform();
-  usart_init(USART1, 9600);
+  usart_init(PFC_USART, 9600);
 
   //Length of chunks being sent in bytes between PFC and MGT
   #define CHUNK_LENGTH 8
@@ -23,10 +23,11 @@ int main(void)
 
 	uint8_t chunk[CHUNK_LENGTH];
   while(1) {
-    memset(chunk, '?', CHUNK_LENGTH);
-   	int read_status = crc_read(USART1, chunk);
-   	if (read_status > 0) {
-   		handle_packet(USART1, chunk);
-   	}
+	  usart_transmitBytes(PFC_USART, "HELLO!!!", 8);
+    // memset(chunk, '?', CHUNK_LENGTH);
+   	// int read_status = crc_read(PFC_USART, chunk);
+   	// if (read_status > 0) {
+   	// 	handle_packet(PFC_USART, chunk);
+   	// }
   }
 }
